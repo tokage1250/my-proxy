ce2012823d9dd0f03e039e36004dc16a79e5b8db
tokage1250 <a.k.maron5@gmail.com>
2026-07-08 20:59:08 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const http = require('http');
 
-// 画面ファイル（HTML）が入っている場所を、Renderに絶対的な場所として教える
-const publicPath = path.join(__dirname, '../public');
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(express.static(publicPath));
-
-// どんなURLが来ても、publicの中のindex.htmlを表示する（これでNot Foundを消す）
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-const port = process.env.PORT || 10000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT || 10000, () => {
+    console.log('Server is running');
 });
