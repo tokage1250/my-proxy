995ebbb626f6e48c5b3b6b6b5981373c4591668f
tokage1250 <a.k.maron5@gmail.com>
2026-07-09 19:25:56 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-const express = require('express');
-const path = require('path');
+import { join } from "node:path";
+import { hostname } from "node:os";
+import { createServer } from "node:http";
+import express from "express";
+import wisp from "wisp-server-node";
+
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+
 const app = express();
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static("./public"));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
 
-// publicフォルダの場所を正確に指定
-const publicPath = path.join(__dirname, '../public');
-app.use(express.static(publicPath));
-
-// どのURLにアクセスされても、必ずトップ画面(index.html)を返す（Not Found対策）
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+// Error for everything else
+app.use((req, res) => {
+	res.status(404);
+	res.sendFile("./public/404.html");
 });
 
-const port = process.env.PORT || 10000;
-app.listen(port, () => {
-    console.log(`サーバーがポート ${port} で起動しました！`);
+const server = createServer();
+
+server.on("request", (req, res) => {
+	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+	app(req, res);
+});
+server.on("upgrade", (req, socket, head) => {
+	if (req.url.endsWith("/wisp/")) {
+		wisp.routeRequest(req, socket, head);
+		return;
+	} 
+	socket.end();
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+	const address = server.address();
+
+	// by default we are listening on 0.0.0.0 (every interface)
+	// we just need to list a few
+	console.log("Listening on:");
+	console.log(`\thttp://localhost:${address.port}`);
+	console.log(`\thttp://${hostname()}:${address.port}`);
+	console.log(
+		`\thttp://${
+			address.family === "IPv6" ? `[${address.address}]` : address.address
+		}:${address.port}`
+	);
+});
+
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+	console.log("SIGTERM signal received: closing HTTP server");
+	server.close();
+	process.exit(0);
+}
+
+server.listen({
+	port,
 });
